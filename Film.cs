b6158911b02a6c6354861f616b8e8052da091898
OrkunDemir5 <orkun.d1999@gmail.com>
2024-11-01 11:56:30 +0300

    Pratik-IMDB-Listesi

---
+++ Film.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratik_IMDB_Listesi
+{
+    public class Film
+    {
+        // Film sınıfının özellikleri
+        public double ImdbPuani { get; set; }
+        public string Isim { get; set; }
+
+        public Film(double imdbPuani, string isim)
+        {
+            ImdbPuani = imdbPuani;
+            Isim = isim;
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿
+using Pratik_IMDB_Listesi;
+
+List<Film> filmler = new List<Film>();
+
+while (true)
+{
+    // Kullanıcıdan film ismi ve imdb puanı alınıyor
+    Console.Write("Film ismi giriniz: ");
+    string filmIsmi = Console.ReadLine();
+
+    Console.Write("Imdb puanını giriniz: ");
+    double imdbPuani;
+    while (!double.TryParse(Console.ReadLine(), out imdbPuani))
+    {
+        Console.WriteLine("Geçerli bir imdb puanı giriniz.");
+    }
+
+    // Alınan bilgilerle film nesnesi oluşturulup listeye ekleniyor
+    filmler.Add(new Film(imdbPuani, filmIsmi));
+
+    // Yeni film eklemek isteyip istemediğini soruyoruz
+    Console.Write("Yeni bir film eklemek istiyor musunuz? (evet/hayir): ");
+    string cevap = Console.ReadLine().ToLower();
+    if (cevap != "evet")
+        break;
+}
+
+Console.WriteLine("--- Tüm Filmler --- ");
+
+// Tüm filmleri listeliyoruz
+foreach (var film in filmler)
+{
+    Console.WriteLine($"Film Adı: {film.Isim}, Imdb Puanı: {film.ImdbPuani}");
+}
+
+Console.WriteLine("\n--- Imdb Puanı 4 ile 9 Arasında Olan Filmler ---");
+// Imdb puanı 4 ile 9 arasında olan filmleri listeleme
+foreach (var film in filmler)
+{
+    if (film.ImdbPuani >= 4 && film.ImdbPuani <= 9)
+    {
+        Console.WriteLine($"Film Adı: {film.Isim}, Imdb Puanı: {film.ImdbPuani}");
+    }
+}
+
+Console.WriteLine("--- 'A' Harfi ile Başlayan Filmler: ---");
+// İsmi 'A' ile başlayan filmleri listeleme
+foreach (var film in filmler)
+{
+    if (film.Isim.StartsWith("A", StringComparison.OrdinalIgnoreCase))
+    {
+        Console.WriteLine($"Film Adı: {film.Isim}, Imdb Puanı: {film.ImdbPuani}");
+    }
+}
+
+Console.WriteLine("Program sona erdi. Çıkmak için bir tuşa basın...");
+Console.ReadKey();
